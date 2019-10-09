6611313202cdb7eee464488065e6080107a8cdc6
mdjamel <mdjaich@gmail.com>
2019-10-09 16:40:25 +0100

    Add Api auth 2

---
+++ app/Http/Resources/UserApiResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserApiResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'api_token' => $this->api_token,
+        ];
+    }
+}
