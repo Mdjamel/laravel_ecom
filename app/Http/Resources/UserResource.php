d16d23d2dfc6e4974a8b62bb4c1fa0420279e64c
mdjamel <mdjaich@gmail.com>
2019-10-10 16:15:41 +0100

    correct add user id to Userresource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,7 +15,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this-id,
+            'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'formattedName' => $this->formattedName(),
