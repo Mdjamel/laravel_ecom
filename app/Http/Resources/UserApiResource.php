5e90a0deb031cd297d291be6a304d9b8d4af213a
mdjamel <mdjaich@gmail.com>
2019-10-10 16:21:44 +0100

    add id to user api resource

--- app/Http/Resources/UserApiResource.php
+++ app/Http/Resources/UserApiResource.php
@@ -15,6 +15,7 @@ class UserApiResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
             'email' => $this->email,
